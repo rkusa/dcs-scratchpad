e41d6d03ee1c14ef807e386eecced5b65ecfe045
SPAS79 <SPAS79@users.noreply.github.com>
2023-12-19 22:58:37 +0100

    Deleted some extras

--- Scripts/Scratchpad/Extensions/sitrep.lua
+++
@@ -1,5 +0,0 @@
--- A button to copy the current mission's situation to the top of the current page
-
-addButton(0, 0, 80, 30, "+ SITREP", function(text)
-  text:insertTop(DCS.getMissionDescription())
-end)
